1cbf13f7083dbbce6ca36a45ad6f321d94cf0c39
donghao <donghao0205@icloud.com>
2019-07-14 21:28:05 +0800

    添加设置类，游戏开始的初始化设置

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,15 +1,22 @@
 import sys
 import pygame
+from settings import Setting
 
 def run_game():
+
+    # 初始化屏幕对象
     pygame.init()
-    pygame.display.set_mode((1200,800))
+    # 创建一个设置类实例
+    settings = Setting()
+    # 设置屏幕
+    screen = pygame.display.set_mode((settings.width,settings.height))
     pygame.display.set_caption("demo")
     while True:
         """监视鼠标和键盘"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        screen.fill(settings.bg_color)
         pygame.display.flip()
 
 
---
+++ settings.py
@@ -0,0 +1,12 @@
+# 游戏初始化设置
+
+import pygame
+
+class Setting():
+    def __init__(self):
+        # 设置屏幕宽度
+        self.width = 1200
+        # 设置屏幕高度
+        self.height = 800
+        # 设置屏幕背景色
+        self.bg_color = (230,230,230)
