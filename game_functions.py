6e3cde6037d12d9b83440458f73fb01b8dc284dd
donghao <donghao0205@icloud.com>
2019-07-15 23:46:38 +0800

    拆分模块，添加移动操作，根据按键进行左右移动

--- game_functions.py
+++ game_functions.py
@@ -2,16 +2,36 @@
 import pygame
 import sys
 
-def check_event():
+
+def check_keydown_event(event,ship):
+    """响应按键"""
+    if event.key == pygame.K_LEFT:
+        ship.moving_left = True
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+
+def check_keyup_event(event,ship):
+    """响应松开"""
+    if event.key == pygame.K_LEFT:
+        ship.moving_left = False
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+
+def check_event(ship):
     """响应键盘和鼠标的事件"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_event(event,ship)
+        elif event.type == pygame.KEYUP:
+            check_keyup_event(event,ship)
 
 def update_screen(ai_settings,screen,ship):
-    """更新飞船的位置"""
+    """更新屏幕上的图像，并切换到新屏幕"""
     # 每次循环都要重新绘制屏幕
     screen.fill(ai_settings.bg_color)
     ship.blitme()
     # 让最近绘制的屏幕可见
     pygame.display.flip()
+
--- ship.py
+++ ship.py
@@ -4,7 +4,7 @@ import pygame
 
 class Ship():
     """初始化飞船并设置其初始位置"""
-    def __init__(self,screen):
+    def __init__(self,ai_settings,screen):
         # 定义surface参数
         self.screen = screen
         # 获取飞船的图片
@@ -12,11 +12,30 @@ class Ship():
         # 获取图片的矩形
         self.rect = self.image.get_rect()
         # 获取屏幕的矩形
-        self.sereen_rect = self.screen.get_rect()
+        self.screen_rect = self.screen.get_rect()
         # 设置飞船和屏幕的相对位置
-        self.rect.centerx = self.sereen_rect.centerx
-        self.rect.bottom = self.sereen_rect.bottom
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+        # 移动标志
+        self.moving_right = False
+        self.moving_left = False
+        # 移动速度
+        self.ai_settings = ai_settings
+        # 在飞船的center中存储小数值
+        self.center = float(self.rect.centerx)
+
 
     def blitme(self):
         self.screen.blit(self.image,self.rect)
 
+    def update(self):
+        """根据移动标志调整飞船位置"""
+        # 更新飞船的center值，而不是rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+
+        # 根据self.centerx 更新rect对象
+        self.rect.centerx = self.center
